ebb51c93d16a480e5f6a6354ffa495598fe48114
Bit-Barron <1bit.baron@gmail.com>
2022-12-24 19:45:51 +0100

    asd

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'if you see the users list go to http://localhost:3000/users';
+    return 'if you see the users list go to https://simplicity-api-ald2.vercel.app/users';
   }
 }
