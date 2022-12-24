881c560c6e8f635c815bd50016182fdc668b7b40
Bit-Barron <1bit.baron@gmail.com>
2022-12-24 19:38:26 +0100

    yxc

---
+++ web/src/lib/elements/Navbar.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+</script>
+
+<div class="bg-[#181a1b]">
+  <div class="container mx-auto p-7">
+    <div class="flex">
+      <div class="text-blue-700 font-bold ml-72">Rest API</div>
+      <a href="/Guide" class="flex ml-96 text-blue-500">Sponsor this project</a>
+
+      <a href="/Guide" class="flex ml-10 text-blue-500">Guide</a>
+    </div>
+  </div>
+</div>
