d069761b32619cfda677420c4ad32ccca409da28
Bit-Barron <1bit.baron@gmail.com>
2022-12-24 14:06:26 +0100

    updata resources

--- web/src/lib/elements/Resources.svelte
+++ web/src/lib/elements/Resources.svelte
@@ -3,10 +3,10 @@
   <div class="mt-6 text-center ml-32">JSONPlaceholder comes with a set of 6 common resources:</div>
   <div class="flex mt-10 justify-center mr-5">
     <div class="text-blue-500 mr-40 ">/Users</div>
-    <div>3 Users</div>
+    <div>8 Users</div>
   </div>
   <div class="flex mr-5 justify-center">
     <div class="text-blue-500 mr-40">/Posts</div>
-    <div>10 Posts</div>
+    <div>8 Posts</div>
   </div>
 </div>
