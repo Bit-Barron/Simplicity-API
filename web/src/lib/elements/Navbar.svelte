77b621dd862d20f3a852e4741194ac81eb506a85
Bit-Barron <1bit.baron@gmail.com>
2022-12-24 12:22:17 +0100

    remove router

--- web/src/lib/elements/Navbar.svelte
+++ web/src/lib/elements/Navbar.svelte
@@ -5,7 +5,7 @@
   <div class="container mx-auto p-7">
     <div class="flex">
       <div class="text-blue-700 font-bold ml-60">Rest API</div>
-      <div class="flex ml-96 text-blue-500">Guide</div>
+      <a href="/guide" class="flex ml-96 text-blue-500">Guide</a>
       <div class="flex ml-3 text-blue-500">Sponsor this project</div>
       <div class="flex ml-3 text-blue-500">Blog</div>
     </div>
