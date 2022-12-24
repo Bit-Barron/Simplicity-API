4f34604ceb437f5aed9a2ab03830e09a86ab112f
Bit-Barron <1bit.baron@gmail.com>
2022-12-24 14:50:01 +0100

    page.svelte

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,18 +1,10 @@
 <script lang="ts">
   import Navbar from '$lib/elements/Navbar.svelte';
   import Tryit from '$lib/elements/Tryit.svelte';
-  import axios from 'axios';
   import Routes from '$lib/elements/Routes.svelte';
   import Resources from '$lib/elements/Resources.svelte';
 
   let showclass = false;
-  let data = [];
-
-  const handler = async () => {
-    const response = await axios.get('http://localhost:3000/users/1');
-    data = response.data;
-    console.log(data);
-  };
 </script>
 
 <div>
@@ -20,7 +12,9 @@
   <div class="">
     <div class="bg-[#182e6e] p-40">
       <div class="text-6xl flex justify-center ml-32 mb-20">Json PLACEHOLDER</div>
-      <div class="flex justify-center mt-5 text-2xl ml-32">Free fake api for testing and prototyping</div>
+      <div class="flex justify-center mt-5 text-2xl ml-32">
+        Free fake api for testing and prototyping
+      </div>
       <div class="flex justify-center mt-5 text-xl ml-32">
         Powered by Json Server + LowDB Tested with XV
       </div>
@@ -44,8 +38,8 @@
     <div>
       <div class="text-center mt-20 mr-32 mb-4 text-3xl">When to use</div>
       <div class="text-center ml-[580px]">
-       this Api placeholder is a free online REST API that you can use whenever you need some fake data.
-        It can be in a README on <br />
+        this Api placeholder is a free online REST API that you can use whenever you need some fake
+        data. It can be in a README on <br />
         <span class="mr-20"
           >GitHub, for a demo on CodeSandbox, in code examples on Stack Overflow, ...or simply to
           test things locally.</span
@@ -55,5 +49,10 @@
     <Resources />
     <Routes />
   </div>
-  <div class="flex justify-center mt-10">Coded and maintained with ❤️ by <span class="text-blue-500 ml-2 mr-2">Bit-Barron</span> © 2022</div>
+  <div class="flex justify-center mt-10">
+    Coded and maintained with ❤️ by <a
+      href="https://github.com/Bit-Barron"
+      class="text-blue-500 ml-2 mr-2">Bit-Barron</a
+    > © 2022
+  </div>
 </div>
